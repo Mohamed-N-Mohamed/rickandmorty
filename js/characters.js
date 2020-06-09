3e837d38761915646e37dd23f3b7e6b3de4ea99a
Mohamed-N-Mohamed <Mohamed-N-Mohamed@outlook.com>
2020-06-09 22:09:09 +0100

    updated files

--- js/characters.js
+++ js/characters.js
@@ -1,6 +1,8 @@
 //DOM
 const display = document.querySelector(".row");
 const next = document.getElementById("next");
+const loader = document.querySelector('.loading');
+
 
 //genrate  id
 let id = 1;
@@ -10,39 +12,46 @@ const characters = "https://rickandmortyapi.com/api/character";
 //get characters
 async function getcharacters() {
   try {
+    display.style.display = 'block'
+    next.style.display = 'inline-block'
     const response = await fetch(characters);
     const data = await response.json();
     const info = await data.info;
     if (info.next !== null) {
       id++;
       fetchNext(info.next, id);
+    } else {
+      UI(data)
     }
     return data;
   } catch (error) {
     console.log(error + "There has been a error");
   }
-  //send to UI
-  // UI(data);
-  // //this will get the other characters
-  // fetchNext(data);
+
 }
 
 //still working on this. Right now its not wroking
 async function fetchNext(url, id) {
+  //make the id to string
   const str = id.toString();
-  
-  const string = url;
-  string.slice(0, -1);
 
-  // const response = await fetch(url);
-  // const data = await response.json();
+  const string = url;
+  //remove the last part and insert genereated id.
+  const newURL = string.slice(0, 48) + str;
+
+  const response = await fetch(newURL);
+  const data = await response.json();
+  //send the data to ui
+  UI(data);
 }
 
 //UI to display characters - episodes - locatioon
 function UI(characters) {
+  console.log(characters)
   const results = characters.results;
   //loop characters
   results.forEach((element) => {
+    console.log(element)
     //create a element
     const div = document.createElement("div");
     div.classList.add("col", "s12", "m4", "card");
@@ -66,9 +75,30 @@ function UI(characters) {
   });
 }
 
-document.addEventListener("DOMContentLoaded", () => {
+//delay
+function delay(e){
+  display.style.display = 'none'
+  next.style.display = 'none'
+  loader.style.display = 'block'
+
+  //rmeove the class after 2 secs
+  setTimeout(() => {
+    loader.style.display = 'none'
+    getcharacters()
+  }, 3000)
+
+
+}
+//call get character
+function cb(){
+  delay();
+}
+
+
+
+window.addEventListener('DOMContentLoaded', () => {
   getcharacters().then((data) => {
     UI(data);
-    next.addEventListener("click", getcharacters);
   });
-});
+  next.addEventListener("click", cb);
+})
